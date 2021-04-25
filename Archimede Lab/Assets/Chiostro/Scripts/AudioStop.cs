77e87d2db83739ea4766405fc1af6f8bb3c4a113
danielemateria <danielemateria14@gmail.com>
2021-04-25 16:37:06 +0200

    25-04-2021

---
+++ Archimede Lab/Assets/Chiostro/Scripts/AudioStop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioStop : MonoBehaviour
+{
+    private AudioSource AS;
+    private bool isPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+        isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pressed()
+    {
+        if (!isPressed)
+            Stop();
+        else
+            Resume();
+    }
+
+    public void Stop()
+    {
+        isPressed = true;
+        AS.Stop();
+        AS.enabled = false;
+    }
+
+    public void Resume()
+    {
+        isPressed = false;
+        AS.enabled = true;
+        AS.Play();
+    }
+}
